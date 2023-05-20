cc4da6ee878151afb5b0cc193745dc02e16f60bf
leekicom <89513120+leekicom@users.noreply.github.com>
2023-05-20 23:54:54 +0900

    tt

--- app.py
+++ app.py
@@ -30,3 +30,5 @@ col1, col2, col3 = st.columns(3)
 col1.metric(label="달러USD", value="1,228 원", delta="-12.00 원")
 col2.metric(label="일본JPY(100엔)", value="958.63 원", delta="-7.44 원")
 col3.metric(label="유럽연합EUR", value="1,335.82 원", delta="11.44 원")
+
+st.text_input("회사명을 입력해주세요")
