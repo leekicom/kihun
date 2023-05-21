a3b59c4e4671e683f40e3d947d6c4b1edf178c59
leekicom <89513120+leekicom@users.noreply.github.com>
2023-05-21 13:51:24 +0900

    dsf

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import csv
 
 
 
 st.title('나의 점수 미리알아보기')
 
 test=st.selectbox('군을 선택해주세요',('','육군(기술행정병)', '해군(기술병)', '공군(기술병)','해병대(기술병)'))
+test1=''
 gstg1=pd.read_excel('군사특기.xlsx','군사특기_육군1')
 gstg2=pd.read_excel('군사특기.xlsx','군사특기_육군2')
 gstg3=pd.read_excel('군사특기.xlsx','군사특기_해군')
@@ -23,8 +23,24 @@ elif test == "해군(기술병)":
   test2=st.selectbox('군사특기를 선택하세요',(gstg3))
 elif test == "공군(기술병)":
   test3=st.selectbox('군사특기를 선택하세요',(gstg4))
-else:
+elif test == "해병대(기술병)":
   test4=st.selectbox('군사특기를 선택하세요',(gstg5))
+else:
+  st.text(test)
+
+index = ['배점']
+columns1 = {'기술자격·면허': [50],'전공학과': [40],'출결상황': [10],'가산점': [15]}
+columns2 = {'기술자격·면허': [90],'출결상황': [10],'가산점': [15]}
+
+if test1=='수송운용(차량운전)':
+  st.title(test1)
+  df = pd.DataFrame(columns2, index=index)
+  st.table(df)
+else:
+  st.title(test1)
+  df = pd.DataFrame(columns1, index=index)
+  st.table(df)
+
 
 
 
