7c7b83f88f37625d9c92a0e07ad7cc802038a5d4
leekicom <89513120+leekicom@users.noreply.github.com>
2023-05-21 09:07:52 +0900

    resr

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import csv
+
 
 
 st.title('나의 점수 미리알아보기')
 
 test=st.selectbox('군을 선택해주세요',('육군(기술행정병)', '해군(기술병)', '공군(기술병)','해병대'))
 st.title(test)
+
+with open('군사특기.csv',newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
