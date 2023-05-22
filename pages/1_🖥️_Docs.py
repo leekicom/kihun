300a3a92da719708fa245a04a5848293a30f8de4
leekicom <89513120+leekicom@users.noreply.github.com>
2023-05-22 12:27:49 +0900

    Update 1_🖥️_Docs.py

--- pages/1_🖥️_Docs.py
+++ pages/1_🖥️_Docs.py
@@ -8,7 +8,7 @@ st.set_page_config(
 
 df=pd.read_excel('육군.xlsx','합격점수')
 
-df_int=df[(df['군사특기명'].str.contains('수송운용')) & (df['입영부대'].str.contains(' 1사단 '))]
+df_int=df[(df['군사특기명'].str.contains('수송운용')) & (df['입영부대'].str.find('1사단'))]
 st.dataframe(df_int)
 chart_data = pd.DataFrame(df_int)
 st.line_chart(data=chart_data, x='입영월', y='총점', width=0, height=0, use_container_width=True)
