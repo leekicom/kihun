abeafe4b0d992c83e70fe3b84c12792388334907
leekicom <89513120+leekicom@users.noreply.github.com>
2023-05-28 10:40:20 +0900

    aa

---
+++ mod2.py
@@ -0,0 +1,21 @@
+K계열전차승무="""<div id="gunView"><div class="bview_btn">
+<div class="right"><span class="button gray3">
+<input type="button" value="목록" onclick="f_setGunData()"></span>
+</div></div>
+<table class="notice_view"><caption>군사특기 대한내용 설명표이며 특기, 병과, 직무개요 및 임무, 관련분야(직접, 간접), 신체사항, 지원자격, 지원부터 배치까지에 대한내용을 제공</caption>
+<colgroup><col class="width_5p"><col class="width_5p"><col class="width_80p"></colgroup><tbody><tr><th scope="row" colspan="2">특기</th><td>K계열전차승무</td></tr><tr><th scope="row" colspan="2">직무개요 및 임무</th>
+<td><img src="/download/mjb/1/CAIS_HPIS_202008050401473360.jpg" alt="K계열전차승무관련 사진" onerror="this.src='/images/duty/army_default.jpg'"><br>K-1 전차승무원으로서 전차를 조종하고, 전차포 사격을 실시하며 전차에 장착된 무전기 동작 및 승무원 정비를 실시
+<br>- 각종지형에서 K-1(A1)전차를 조종(주간,야간)
+<br>- 전차포 사격 및 공용화기 사격
+<br>- 전차 탄약적재 및 탄약 장전 실시
+<br>- 전차에 장착된 통신 장비를 조작
+<br>- 전차차체 및 포탑에 대한 승무원 정비를 실시</td></tr><tr><th scope="rowgroup" rowspan="2">관련분야</th><th scope="row">직접</th><td>기갑분야(전체)</td></tr><tr><th scope="row">간접</th><td>전계열분야</td></tr><tr><th scope="row" colspan="2">신체사항</th><td>- 신체등급 2급 이상('22년 2월 입영부터는 신체등급 3급 이상으로 적용)
+<br>- 신장 164cm ~ 180cm 이내('22년 2월 입영부터는 신장 159cm ~ 187cm 이내로 적용)
+<br>- 나안시력 0.2 이상('22년 2월 입영부터는 교정시력 0.8 이상으로 적용)
+<br>- 디스크/관절장애, 운동장애, 수지결손, 수지강직, 색각장애, 청력장애, 언어장애, 폐쇄공포증, 수전증, 2차 심리검사 결과 등 정밀검사 대상자 제외</td></tr><tr><th scope="row" colspan="2">지원자격</th><td>○ 연령 : 지원서 접수년도 기준 18세이상 28세이하
+<br>○ 신체등급 1~2급을 판정받은 현역입영대상자(신체등급 1 ~3급을 판정받은 현역입영 대상자 : '22년 2월 입영부터 적용)
+<br>○ 자격/면허를 소지하였거나 자격/면허 미소지한 경우는 해당 전공학과 전문계 고교 3년이상 수료 또는 대학 1년재학 이상인 사람
+<br>○ 수형사유 지원제한 요건
+<br>◎ 금고이상의 형(집행유예 포함)을 선고받은 사람
+<br>◎ 재판중이거나 수사가 종결되지 아니한 사람</td></tr><tr><th scope="row" colspan="2">지원부터 배치까지</th><td>▶지원 → 1차 선발(합격)자 발표 → 면접 및 신체검사 → 최종선발(합격)자 발표 → 입영 → 기초군사훈련(5주) → 병과교육(기계교 : 3주) → 부대배치 조회(육군본부 홈페이지 - 신병부대배치 화면에서도 가능)
+<br>* 육군훈련소 입영일자에 따라 병과학교 미교육자 있을 수 있습니다.</td></tr></tbody></table><div class="bview_btn"><div class="right"><span class="button gray3"><input type="button" value="목록" onclick="f_setGunData()"></span></div></div></div>"""
--- pages/5_test.py
+++ pages/5_test.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 import re
 import webbrowser
+import mod2
 
 moon=open('speech_moon.txt',encoding='UTF-8').read()
 moon=re.sub('[^가-힣]',' ',moon)
@@ -14,36 +15,7 @@ if st.button('Say hello'):
     webbrowser.open('http://www.naver.com')
 else:
     st.write('Goodbye')
-html = """
-    <table class="table_col">
-	<thead>
-		<tr>
-			<th rowspan="2" scope="col">구분</th>
-			<th colspan="4" scope="col" align="center">1차 평가</th>
-			<th scope="col" align="center">2차 평가</th>
-			<th rowspan="2" scope="col" align="center">계</th>
-		</tr>
-		<tr>
-			<th scope="col">기술자격·면허</th>
-			<th scope="col">전공학과</th>
-			<th scope="col">출결상황</th>
-			<th scope="col">가산점</th>
-			<th scope="col" align="center">면접</th>
-		</tr>
-	</thead>
-	<tbody class="text_center">
-		<tr>
-			<th scope="row">배점</th>
-			<td align="center">50</td>
-			<td align="center">40</td>
-			<td align="center">10</td>
-			<td align="center">15</td>
-			<td>적격/부적격</td>
-			<td>115</td>
-		</tr>
-	</tbody>
-</table>
-"""
-st.markdown(html, unsafe_allow_html=True)
+mod2_html=mod2.K계열전차승무
+st.markdown(mod2_html, unsafe_allow_html=True)
 # dic_word=df_word.set_index('word').to_dict()['n']
 # dic_word
