3968fe5578a997f508c8f3f450d620d139f80460
leekicom <89513120+leekicom@users.noreply.github.com>
2023-06-02 21:57:48 +0900

    qq

--- airforce.py
+++ airforce.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import os
-import mod2
+import mod1
 from PIL import Image
 
 
@@ -35,6 +35,19 @@ def gun_query():
                 columns = cols
             )  
     return results_df
+def jeomsu_query(a,b):
+    query = "select 점수 from 공군점수계산 where 구분='"+a+"' and 자격증구분='"+b+"'"
+    conn = create_connection("mydatabase.db")
+
+    query = conn.execute(query)
+    cols = [column[0] for column in query.description]
+    results_df1= pd.DataFrame.from_records(
+                data = query.fetchall(), 
+                columns = cols
+            )  
+    sum1=int(results_df1.values)
+
+    return sum1
 def air_force_hh():
     st.header("공군 군사특기별 현황")
     df1=gun_query()
@@ -82,7 +95,108 @@ def sogae_query(b):
             )  
     return results_df       
 def air_force_jeomsu():
+    sum1=0
+    sum2=0
+    sum3=0
+    sum4=0
+    st.header("나의 점수 미리알아보기")
     df1=gun_query()
+    code_df=code_query()
+    test1=st.selectbox('군사특기를 선택하세요',code_df['소분류'].drop_duplicates(keep='first'),0)
+    df3=code_df.query("소분류=='"+test1+"'")
+
+    result3=df3['대분류'].values
+    result4=df3['코드'].values
+    txt3=''.join(result3)
+    txt4=''.join(result4)
+    df2=df1.query("특기=='"+txt3+"'")
+    if txt3 in ('일반'):
+        st.markdown(f":blue[{txt3}]")
+        mod1_html=mod1.html7
+        st.markdown(mod1_html, unsafe_allow_html=True)
+        st.markdown("1.자격.면허/배점 70점")
+        mod1_html=mod1.html8
+        image1=st.markdown(mod1_html, unsafe_allow_html=True)
+        conn = create_connection("mydatabase.db")
+        query = conn.execute("select 자격증구분 from 공군점수계산 where 구분='일반자격'")
+
+        cols = [column[0] for column in query.description]
+        results_df1= pd.DataFrame.from_records(
+                data = query.fetchall(), 
+                columns = cols
+                )  
+        license1="일반자격"
+        license2=st.selectbox('자격증구분을 선택하세요',results_df1)
+        sum1=jeomsu_query(license1,license2)  
+
+        if txt3!='' and sum1!=0:
+            image1.empty()
+            sum1_a=st.markdown(f"1. :green[{sum1}]점")
+            st.markdown("2. 고교 출결사항 20점")
+            mod1_html=mod1.html9
+            image2=st.markdown(mod1_html, unsafe_allow_html=True)
+
+            query = conn.execute("select 자격증구분 from 공군점수계산 where 구분='일반결석일'")
+            cols = [column[0] for column in query.description]
+            results_df1= pd.DataFrame.from_records(
+                    data = query.fetchall(), 
+                    columns = cols)  
+            hak2=st.selectbox('결석일자를 선택하세요',results_df1)
+            hak1="일반결석일"
+            sum2=jeomsu_query(hak1,hak2)
+            if txt3!='' and sum1!=0 and sum2!=0:
+                image2.empty()
+                sum1_a.empty()
+                sum3_a=st.markdown(f"1. :green[{sum1}]점,2. :green[{sum2}]점")
+                st.markdown("3. 가산점/배점 15점(접수마감일 기준)")
+                mod1_html=mod1.html10
+                image4=st.markdown(mod1_html, unsafe_allow_html=True)
+
+                options = st.multiselect(
+                '가산점이 있으면 추가하세요.',['병역진로설계 지원자','다자녀(3) 가정 자녀','다자녀(2) 가정 자녀','지정특기(방공포,군사경찰,조리)','화생방 직종(운전면허-2종보통이상)','항공정비사(기사급) 또는 항공정비 기초인력 인증서',
+                '한국사능력검정(3,4급)','한국사능력검정(1,2급)','한국어능력검정(3,4급)','한국어능력검정(1,2급)','토익(520~),토플(59~),텝스(201~)','토익(730~),토플(82~),텝스(277~)',
+                '질병치료에 따른 병역처분변경','국외이주자 중 현역병복무지원자','독립유공자 손·자녀 또는 국가유공자 자녀', '생계급여 수급권자'],['병역진로설계 지원자'])
+                for i in options:
+                    if i in ('독립유공자 손·자녀 또는 국가유공자 자녀','생계급여 수급권자','다자녀(3) 가정 자녀','질병치료에 따른 병역처분변경','국외이주자 중 현역병복무지원자','지정특기(방공포,군사경찰,조리)','화생방 직종(운전면허-2종보통이상)','항공정비사(기사급) 또는 항공정비 기초인력 인증서'):
+                        sum4=sum4+4
+                    elif i in ('모집특기경력(2년 이상)'):
+                        sum4=sum4+3
+                    elif i in ('다자녀(2) 가정 자녀','모집특기경력(1년~2년 미만)','한국사능력검정(1,2급)','한국어능력검정(1,2급)','토익(730~),토플(82~),텝스(277~)'):
+                        sum4=sum4+2
+                    else :
+                        sum4=sum4+1
+
+                query = conn.execute("select 자격증구분 from 점수계산 where 구분='헌혈'")
+                cols = [column[0] for column in query.description]
+                results_df1= pd.DataFrame.from_records(
+                    data = query.fetchall(), 
+                    columns = cols)  
+                blood1=st.selectbox('헌혈횟수를 선택하세요',results_df1)
+                sum41=jeomsu_query('헌혈',blood1)
+
+                query = conn.execute("select 자격증구분 from 점수계산 where 구분='봉사시간'")
+                cols = [column[0] for column in query.description]
+                results_df1= pd.DataFrame.from_records(
+                    data = query.fetchall(), 
+                    columns = cols)  
+                blood2=st.selectbox('봉사시간을 선택하세요',results_df1)
+                sum42=jeomsu_query('봉사시간',blood2)
+                if sum41+sum42>8 :
+                    sum4=sum4+8
+                else:
+                    sum4=sum4+sum41+sum42
+                if sum4>15:
+                    sum4=15
+
+                if test1!='' and sum1!=0 and sum2!=0 and sum4>1:
+                    image4.empty()
+                    sum3_a.empty()
+                    sumtotal=sum1+sum2+sum4
+                    st.subheader(f"1. :green[{sum1}]점,2. :green[{sum2}]점,3. :green[{sum4}]점 합계 : :violet[{sumtotal}]점")
+                    st.subheader(test1)
+                    mod1_html=mod1.html6
+                    st.markdown(mod1_html, unsafe_allow_html=True)
+
 page_names_to_funcs = {
     "군사특기별 현황": air_force_hh,
     "나의점수 알아보기": air_force_jeomsu,
--- mod1.py
+++ mod1.py
@@ -321,3 +321,196 @@ html5="""<ul class="list">
 	<li>질병치료에 따른 병역처분변경으로 현역병입영대상 판정자 : 4점</li>
 </ul>"""
 html6="""<a href="https://mma.go.kr/conscription/recruit_service/procedure/army/S_board_text.do?mc=mma0000388&gun_gbcd=1&mojip_gbcd=1" target="_blank">군사특기임무 및 설명</a>"""
+html7="""<div class="layout_h4">
+<h4>&nbsp;일반기술병(일반직종)</h4>
+
+<table class="table_col">
+	<thead>
+		<tr>
+			<th rowspan="2" scope="col">구분</th>
+			<th colspan="3" scope="col">서류전형(1차)</th>
+			<th rowspan="2" scope="col">면접<br>
+			(2차)</th>
+			<th rowspan="2" scope="col">계</th>
+		</tr>
+		<tr>
+			<th scope="col">자격/면허&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+			<th scope="col">출결</th>
+			<th scope="col">가산점</th>
+		</tr>
+	</thead>
+	<tbody class="text_center">
+		<tr>
+			<th scope="row">배점</th>
+			<td>70</td>
+			<td>20</td>
+			<td>15</td>
+			<td>35</td>
+			<td>140</td>
+		</tr>
+	</tbody>
+</table>
+</div>"""
+html8="""<div class="layout_h4">
+<table class="table_col">
+	<thead>
+		<tr>
+			<th rowspan="2" scope="row">구분</th>
+			<th colspan="3" scope="col">국가기술자격증</th>
+			<th colspan="3" scope="col">일학습병행 자격증</th>
+			<th colspan="2" scope="col">일반자격증</th>
+			<th rowspan="2" scope="col">미소지</th>
+		</tr>
+		<tr>
+			<th scope="col">기사 이상</th>
+			<th scope="col">산업기사</th>
+			<th scope="col">기능사</th>
+			<th scope="col">L6, L5</th>
+			<th scope="col">L4, L3</th>
+			<th scope="col">L2</th>
+			<th scope="col">공인</th>
+			<th scope="col">비공인</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th scope="row">배점</th>
+			<td style="text-align: center;">70</td>
+			<td style="text-align: center;">68</td>
+			<td style="text-align: center;">66</td>
+			<td style="text-align: center;">70</td>
+			<td style="text-align: center;">68</td>
+			<td style="text-align: center;">66</td>
+			<td style="text-align: center;">64</td>
+			<td style="text-align: center;">62</td>
+			<td style="text-align: center;">60</td>
+		</tr>
+	</tbody>
+</table>
+</div>"""
+html9="""<div class="layout_h4">
+<table class="table_col">
+	<thead>
+		<tr>
+			<th rowspan="2" scope="col">구분</th>
+			<th colspan="5" scope="col">결석일자</th>
+			<th rowspan="2" scope="col">비고</th>
+		</tr>
+		<tr>
+			<th scope="col">0일</th>
+			<th scope="col">1~2일</th>
+			<th scope="col">3~4일</th>
+			<th scope="col">5~6일</th>
+			<th scope="col">7일 이상</th>
+		</tr>
+	</thead>
+	<tbody class="text_center">
+		<tr>
+			<th scope="row">배점</th>
+			<td>20</td>
+			<td>19</td>
+			<td>18</td>
+			<td>17</td>
+			<td>16</td>
+			<td>고교3년간 누계적용</td>
+		</tr>
+	</tbody>
+</table>
+</div>"""
+html10="""<div class="layout_h3">
+<h3>가산점 : 배점15점</h3>
+
+<ul class="list">
+	<li>국가 유공자 자녀, 독립유공자 자녀/손자녀 가산점 : 4점</li>
+	<li>질병치유 자진입대 가산점 : 4점
+	<ul>
+		<li><strong><span style="color: rgb(0, 0, 205);">전시근로역에서 현역병입영대상으로 역종이 변경된 사람.&nbsp; 다만, 보충역은 2021년 10월 13일 이전에 현역병 입영대상으로 역종이 변경된 사람에 한하여 인정</span></strong></li>
+	</ul>
+	</li>
+	<li>국외이주자 중 현역복무지원자 가산점 : 4점
+	<ul>
+		<li>국외이주자 인정조건(영주권자, 복수국적자, 재외국민 등)과 제출서류 안내는 병적을 관리하는 지방병무청 국외이주 업무담당에게 문의하시기 바랍니다.</li>
+	</ul>
+	</li>
+	<li>다자녀(3명 이상) 가정자녀 가산점 : 4점 /<strong><span style="color: rgb(0, 0, 205);"> 다자녀(2인) 가정자녀 : 2점 </span></strong></li>
+	<li>경제적 약자 지원대상(생계급여 수급자) : 4점</li>
+	<li>사회봉사활동 경력(접수마감일 기준 최근 1년 이내) :&nbsp;최대8점 (헌혈과 봉사실적을 통합운영)
+	<ul>
+		<li>사회봉사 : 8시간 이상 1점/ 16시간&nbsp;이상 2점/ 24시간 이상 3점/ 32시간 이상 4점/40시간 이상 5점/48시간 이상 6점/ 56시간 이상 7점/ 64시간 이상 8점</li>
+		<li>헌혈 : 1회 1점/ 2회 2점/ 3회 3점/ 4회 4점/ 5회 5점/ 6회 6점/ 7회 7점 /8회 8점
+		<ul>
+			<li>사회봉사활동 및 헌혈 경력은 회차별 지원서 마감일을 기준으로 기간산정</li>
+			<li><strong><span style="color: rgb(0, 0, 255);">헌혈은 헌혈 점수를 부여하며, 사회봉사시간으로 점수부여를 하지 않습니다</span></strong>.<strong><span style="color: rgb(0, 0, 205);"> (중복 불가)</span></strong></li>
+			<li>사회봉사활동 경력은 '사회복지사업법'&nbsp;또는 "자원봉사활동기본법' 또는 '청소년활동진흥법'에서 수요처로 지정된 기관에서 실시한 봉사활동만 인정되며, '사회복지 자원봉사 인증관리(VMS) 홈페이지'&nbsp;또는 '1365자원봉사포털'&nbsp;또는&nbsp;'청소년자원봉사포털(DOVOL)'에서 발급하는 봉사활동 증명서로 확인</li>
+		</ul>
+		</li>
+	</ul>
+	</li>
+	<li>지정특기(방공포, 군사경찰, 조리) 가산점 : 4점
+	<ul>
+		<li>일반 직종 지원자 중&nbsp;방공포, 군사경찰, 조리 특기 희망자</li>
+	</ul>
+	</li>
+	<li>화생방 직종(운전면허소지자-2종보통이상<strong><span style="color: rgb(0, 0, 205);">(오토제외</span></strong>)) 가산점 : 4점</li>
+	<li>항공정비사(기사급) 또는 항공정비 기초인력 인증서 소지자 : 4점
+	<ul>
+		<li>기계직종(항공기기체정비, 항공기기관정비 특기 희망자) 지원자 중 항공정비사 또는 항공정비 기초인력 인증서 소지자</li>
+		<li>통신전자전기직종(항공전자장비정비 특기 희망자) 지원자 중 항공정비사 또는 항공정비 기초인력 인증서 소지자</li>
+		<li>항공정비사 자격증에<strong><span style="color: rgb(0, 0, 255);"> FAA A&amp;P (미항공연방청 항공정비사 자격증) 인정</span></strong></li>
+	</ul>
+	</li>
+	<li>병역진로설계 군 추천특기 지원자 가산점 : 1점
+	<ul>
+		<li><strong><span style="color: rgb(0, 0, 255);">추천받은 특기와 모집병 지원 특기 일치시 가산점 부여</span></strong>(지원서 접수 마감일 기준 최종 직업선호도 검사 결과,</li>
+		<li>추천받은특기가 적용되며, 추천특기와 1차 선발 특기가 일치하는 경우 적용 됨)</li>
+	</ul>
+	</li>
+	<li>한국사능력검정(접수마감일 기준 최근 4년 이내, 국사편찬위원회) : 2점
+	<ul>
+		<li>3, 4급 1점/ 1, 2급 2점</li>
+	</ul>
+	</li>
+	<li>한국어능력검정(접수마감일 기준&nbsp;최근 2년 이내, KBS) : 2점
+	<ul>
+		<li>3,&nbsp;4급 1점/ 1, 2급&nbsp;2점</li>
+	</ul>
+	</li>
+	<li>공인 영어성적(접수마감일 기준 2년 이내 성적- <strong><span style="color: rgb(0, 0, 205);">토익/토플 경우 국내/국외 정기시험만 가능, 특별/수시시험 불가</span></strong>)
+	<ul>
+		<li><strong><span style="color: rgb(0, 0, 255);">텝스성적기준은</span><span style="color: rgb(128, 0, 128);"> '21년 점수환산표' </span><span style="color: rgb(0, 0, 255);">적용에 따라 변경 ('22년 1회차('22년 4월입영) 부터 적용)</span></strong>
+		<ul>
+			<li>영어어학병은 가산점 부여 제외
+			<table class="table_col" style="width: 629px; height: 108px;">
+				<caption>영어어학병 가선점 부여 제외에 대한 표이며 구분/배점, 1점, 2점에 대한내용을 제공</caption>
+				<thead>
+					<tr>
+						<th scope="row">구분 / 배점</th>
+						<th scope="col">1점</th>
+						<th scope="col">2점</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">TOEIC</th>
+						<td style="text-align: center;">520 ~ 729</td>
+						<td style="text-align: center;">730 이상</td>
+					</tr>
+					<tr>
+						<th scope="row">TOEFL</th>
+						<td style="text-align: center;">59 ~ 81</td>
+						<td style="text-align: center;">82 이상</td>
+					</tr>
+					<tr>
+						<th scope="row"><strong><span style="color: rgb(0, 0, 255);">New&nbsp;TEPS</span></strong></th>
+						<td style="text-align: center;"><strong><span style="color: rgb(0, 0, 255);">201 ~276</span></strong></td>
+						<td style="text-align: center;"><strong><span style="color: rgb(0, 0, 255);">277&nbsp;이상</span></strong>&nbsp;&nbsp;</td>
+					</tr>
+				</tbody>
+			</table>
+			</li>
+		</ul>
+		</li>
+	</ul>
+	</li>
+</ul>
+</div>"""
