84aae4e30aa7f0c67421a8b068101f2a4f6b289d
leekicom <89513120+leekicom@users.noreply.github.com>
2024-02-29 10:01:16 +0900

    ss

--- job.py
+++ job.py
@@ -10,11 +10,16 @@ from PIL import Image
 def emp_hg():
     st.header("직업선호도 유형")
     df1=mod2.emp1_query('직업선호도')
+    dfa=df1.query("군별=='육군'")
+    dfb=df1.query("군별=='해군'")  
+    dfc=df1.query("군별=='공군'") 
+    dfd=df1.query("군별=='해병'")    
     col1, col2 = st.columns(2) 
     with col1:
         test1=st.selectbox('군을 선택하세요',df1['군별'].drop_duplicates(keep='first'),0)
-    with col2:    
-        test2=st.selectbox('특기를 선택하세요',df1['특기'].drop_duplicates(keep="first"),0)
+    with col2:  
+        df3=df1.query("군별=='"+test1+"'") 
+        test2=st.selectbox('특기를 선택하세요',df3['특기'].drop_duplicates(keep="first"),0)
     df2=df1.query("군별=='"+test1+"' & 특기=='"+test2+"' ")
     restult1=df2[['특기','현실형','탐구형','예술형','사회형','진취형','관습형']]
     restult1.set_index(['특기'],inplace=True)
